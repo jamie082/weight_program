8ef9c0073ed52e8406fa003e6fa6800d5b4b7817
jamie082 <jamiemorrissey02@gmail.com>
2019-04-01 16:28:50 -0700

    fixed master branch (default source code file

--- weight_0919.c
+++ weight_0919.c
@@ -16,6 +16,7 @@ int main(void)
 	int month, day, year;
 
 	printf("Weight Management v0.5\n");
+
 	printf("Enter weight: ");
 	scanf("%f", &weight);
 
@@ -23,7 +24,7 @@ int main(void)
 	scanf("%d/%d/%d", &month, &day, &year);
 
 	fseek(fp, 1, SEEK_END);
-	fprintf(fp, "%d/%d/%d\t\t%.1f\n", month, day, year, weight);
+	fprintf(fp, "%d/%d/%d\t\t%.2f\n", month, day, year, weight);
 
 	fclose(fp);
 

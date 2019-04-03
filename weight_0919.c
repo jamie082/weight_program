5c75382cd1d7bec6986579088b856a6e57fa5c5e
jamie082 <jamiemorrissey02@gmail.com>
2019-04-03 09:24:17 -0700

    added string support for scanf on weight variable

--- weight_0919.c
+++ weight_0919.c
@@ -1,6 +1,7 @@
 // weight_logger.c
 
 #include <stdio.h>
+#include <stdlib.h>
 
 void my_function(void);
 
@@ -8,23 +9,61 @@ int main(void)
 {
 	FILE *fp;
 
-	// correct text file name
+	char filename[20] = "weight_log.txt";
 
-	fp = fopen("weight_log.txt", "a");
+	fp = fopen(filename, "a");
 
-	float weight;
+	if((fp = fopen(filename, "a")) == NULL)
+	{
+		fprintf(stderr, "Error opening file.");
+		exit(1);
+	}
+
+	char input[15];
 	int month, day, year;
 
 	printf("Weight Management v0.5\n");
+	
+	// if weight is a negative number or is not a digit
 
-	printf("Enter weight: ");
-	scanf("%f", &weight);
+	int x;
+
+	char *ptr;
+	int ret;
+
+	long number_2;
+	static int a;
+
+	do
+	{
+		printf("Enter weight: ");
+		scanf("%s", input);
+
+		// if i.e. "205.2a" or "-205.2" is entered, truncate,
+		ret = strtol(input, &ptr, 10);
+
+		number_2 = ret;
+		
+		x = 1;
+		// printf("%ld\n", number_2);
+		
+		// abs() absolute value of number
+		// labs() return absolute value of long integer
+		
+		a = labs(number_2);
+
+		printf("(%d lbs)\n", a);
+	} while (x != 1);
 
 	printf("Enter date: ");
 	scanf("%d/%d/%d", &month, &day, &year);
 
+	printf("(%d/%d/%d)\n", month, day, year);
+
 	fseek(fp, 1, SEEK_END);
-	fprintf(fp, "%d/%d/%d\t\t%.2f\n", month, day, year, weight);
+	fprintf(fp, "%d/%d/%d\t\t%d\n", month, day, year, a);
+
+	printf("Successfully wrote to file %s\n", filename);
 
 	fclose(fp);
 
